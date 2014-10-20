ac7bcfb50ffeaa3c5b59b04d34935e2057d218da
Siwei <shensiwei@sina.com>
2014-10-20 16:43:46 +0800

    修改了默认的root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  root 'landing_pictures#admin_index'
+  root 'landing_pictures#index'
   resources :user_account_records
   resources :landing_pictures do
     collection do
