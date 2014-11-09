b5c7c08f57bfe3ef33674991667dac72a96c2219
Siwei <shensiwei@sina.com>
2014-11-10 07:49:54 +0800

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20141027083705) do
 
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.string   "link_to"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "ckeditor_assets", force: true do |t|
     t.string   "data_file_name",               null: false
     t.string   "data_content_type"
